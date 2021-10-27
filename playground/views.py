82bd0ff52e418e1ace83bc954591db97dcb2f1b8
Theseregrets <rishitashaw49@gmail.com>
2021-10-27 12:51:27 +0530

    initial commit

---
+++ playground/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+
+# Create your views here.
+
+
+def calculate():
+    x = 1
+    y = 1
+    return x+y
+
+
+def say_hello(request):
+    x = calculate()
+    return render(request, 'hello.html')
