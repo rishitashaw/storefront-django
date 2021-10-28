05019ede8ef0a79d7894c6607e1c62cfb7548046
Theseregrets <rishitashaw49@gmail.com>
2021-10-28 14:42:05 +0530

    connecting to mysql db

--- store/models.py
+++ store/models.py
@@ -16,8 +16,9 @@ class Collection(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField()
     descriptions = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=3)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=3)
     inventory = models.IntegerField()
     last_updated = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(
