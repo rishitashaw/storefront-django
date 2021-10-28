c731df539664786de6e736c5ac80bd932ab9f7ff
Theseregrets <rishitashaw49@gmail.com>
2021-10-28 18:04:30 +0530

    db connected

--- storefront/settings.py
+++ storefront/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'playground',
     'debug_toolbar',
     'store',
-    'tags'
+    'tags',
 ]
 
 MIDDLEWARE = [
@@ -85,8 +85,11 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'skvsarkc3579'
     }
 }
 
